9ba500181f60b92d71a97e3e1cff122dfd5486ab
Prince Christian Basiga <princebasiga@gmail.com>
2018-05-16 02:15:53 -0700

    Added dropdown for color selection and pulled avaialble color data from database

--- ces_website/src/3DPrinterPage.js
+++ ces_website/src/3DPrinterPage.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import fire from './back-end/fire';
 import Dropzone from 'react-dropzone';
 import './3DPrinterPage.css'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+
+const url ="https://middleman2.herokuapp.com"
+
 class PrintingPage extends Component{
 
     constructor(props){
@@ -16,22 +20,42 @@ class PrintingPage extends Component{
             dropZoneText : "Drag your Model Here or Click to Upload"
         }
         this.uploadFile = this.uploadFile.bind(this);
+        this.toggleColorDD = this.toggleColorDD.bind(this);
     }
 
     componentWillMount(){
         //Before this component is mounted on screen want to make sure colors
         //there will be set interval to update the queue, will determine that time later
         //colors avaialbe have to be refreshed, don't want that dynamic.
+        this.setState({
+            colors : ["red","blue"]
+        })
 
-
+        this.pullAvailableColors();
     }
 
-    updateAvailableColors(){
+    pullAvailableColors(){
 
-        const database = fire.database();
+        //Honestly this one doesn't need to be secure and can just be done directly
+        //Need to rewwork rules first, but yeah, read for colors should be public
+        //write is something admin only though.
+        /*
+        const response = await fetch(url+"/availablecolors",{
+            method:"GET",
+            mode:'no-cors',
+        })
+            .then(res => {if (res){
 
-
-
+                this.setState({
+                    //Initializes states colors to colors pulled from database
+                    colors : res.json().colors
+                })
+            }})
+*/
+            fire.database().ref("PrinterState/Color").once('value')
+                .then(snapshot => {this.setState({
+                    colors:snapshot.val().split(",")
+                })})
     }
 
     uploadFile(event){
@@ -46,7 +70,7 @@ class PrintingPage extends Component{
         const auth = fire.auth();
         //This was good learning, and will require this sign in for client side.
         //but prio is sending get request
-       /* auth.signInWithEmailAndPassword("princebasiga@gmail.com","Cplus2x2Unity")
+       /* auth.signInWithEmailAndPassword("bleh@gmail.com","bleh")
             .then(user => {if (user) {
 
                 const storage = fire.storage().ref("3DPrinterQueue/"+this.state.fileUploaded.name);
@@ -55,6 +79,9 @@ class PrintingPage extends Component{
 
                 task.on("state_changed",
     
+                //Main thing though is no progress on front-end
+                //to show user.
+
                 function progress(snapshot){
                     var percentage = snapshot.bytesTransferrd / snapshot.totalBytes;
                     percentage *= 100;
@@ -77,7 +104,9 @@ class PrintingPage extends Component{
       */
      this.uploadSecurely()
 
-            .then( res => {console.log(res);})
+            .then( res => {this.setState({
+                dropZoneText:"Drag your Model Here or Click to Upload"
+            })});
         
     }
 
@@ -86,7 +115,7 @@ class PrintingPage extends Component{
         const data = new FormData();
         data.append('file', this.state.fileUploaded);
         //Will put in url of other server here
-        const response = await fetch("https://middleman2.herokuapp.com/uploadFile", {
+        const response = await fetch(url+"/uploadFile", {
 
 
             method:"POST",
@@ -121,13 +150,34 @@ class PrintingPage extends Component{
         });
     }
 
+    toggleColorDD(){
+
+        console.log("hello");
+        this.setState({
+            colorDropDown: !this.state.colorDropDown
+        });
+    }
+
     render(){
 
         return (
-            //This will have the list of things in queue, prob like side scroll thing
-            //Form to add to queue
+           
             <div>
-                
+
+                <Dropdown className="ColorPicker" isOpen={this.state.colorDropDown} toggle={this.toggleColorDD}>
+
+                        <DropdownToggle caret>
+                            Color
+                        </DropdownToggle>
+
+                        <DropdownMenu>
+
+                            {this.state.colors.map(color => {
+                                return <DropdownItem> {color} </DropdownItem>
+                            })} 
+
+                        </DropdownMenu>
+                </Dropdown>
                 <Dropzone className="DropZone" onDrop={this.onDrop.bind(this)}>
                     <p>{this.state.dropZoneText}</p>
                 </Dropzone>
