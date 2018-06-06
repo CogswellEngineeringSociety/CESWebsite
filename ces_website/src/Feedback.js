5eae5ebe6e8bac7481e6cbc080558cd3de03ea19
Prince Christian Basiga <princebasiga@gmail.com>
2018-06-06 02:45:44 -0700

    Pull category list from keys in database. Implemented shouldComponentUpdate

--- ces_website/src/Feedback.js
+++ ces_website/src/Feedback.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Form,Label,Input,Dropdown,DropdownItem,DropdownToggle,DropdownMenu} from 'reactstrap';
+import fire from './back-end/fire';
 /*
 
 This page will have simple form. Topic and description.
@@ -12,8 +13,8 @@ class FeedbackPage extends Component{
         super(props);
 
         this.state = {
-            category:"",
-            categories : [],
+            category:"Unspecified",
+            categories : ["Unspecified"],
             categoryMenuOpen : false,
             topic:"",
             description:""
@@ -22,6 +23,35 @@ class FeedbackPage extends Component{
         this.onUpdateField = this.onUpdateField.bind(this);
         this.toggleCategory = this.toggleCategory.bind(this);
         this.setCategory = this.setCategory.bind(this);
+    }
+
+    pullCategories(){
+        //This won't be called every frame, it's not as urgent, but should be updated according to database.
+        //The categories are keys for all entries, so it's not like just plopping categories themselves just to do this
+        //but it is beneficial that don't gotta reach in code to change this, but not execessive since already there.
+        const dbRef = fire.database().ref("Feedback/");
+        var categs = []
+        dbRef.once('value')
+            .then(snapshot => {
+
+                snapshot.foreach(value => {
+                    categs.push(value);
+                })
+                categs.push("Unspecified");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    componentWillMount(){
+        this.pullCategories();
+    }
+    shouldComponentUpdate(newProps, newState){
+
+        
+        return newState.category != this.state.category || newState.topic != this.state.topic || 
+        newState.description != this.state.description || newState.categoryMenuOpen != this.state.categoryMenuOpen;
     }
 
     onUpdateField(event){
