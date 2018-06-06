fc8e226279efb44ac2d0ca4ad6832d98b9147d7e
Prince Christian Basiga <princebasiga@gmail.com>
2018-06-05 19:37:01 -0700

    Added NewsEntry Component, pulled news entries from database and only re-render when amount of entries has changed

--- ces_website/src/News.js
+++ ces_website/src/News.js
@@ -1,12 +1,97 @@
 import React, {Component} from 'react'
 
+import fire, {url} from './back-end/fire';
 
+//Reading news in will be public to all, writing news will be done through post request to the admin app
 class NewsPage extends Component{
 
 
+    constructor(props){
+        super(props);
+
+        //State of news page will just be sorting and filter.
+        //Could embed these settings in user profiles, but prob makes more sense to just keep in state
+        this.state = {
+            sortCriterion : null,
+            filter:null,
+            newsEntries: []
+        }
+
+        this.pullNewsEntries = this.pullNewsEntries.bind(this);
+        setInterval(this.pullNewsEntries,1000);
+    }
+    
+    shouldComponentUpdate(nextProps,nextState){
+
+        if (nextState.sortCriterion != this.state.sortCriterion || nextState.filter != this.state.filter){
+            return true;
+        }
+        else if (nextState.newsEntries.length != this.state.newsEntries.length){
+            return true;
+        }
+        
+        return false;
+
+    }
+
+    componentWillUpdate(){
+        
+        //This should only be called once.
+        console.log("I'm called");
+    }
+
+    pullNewsEntries(){
+
+
+        console.log("pulling new entries");
+        //Then fetch from the news all that match the filter / criterion. 
+        //For now just all
+        const dbRef = fire.database().ref("News/");
+
+
+        var data = []
+        
+        dbRef.once('value')
+        .then( (snapshot) => {
+
+            snapshot.forEach(child => {
+                const newsData = child.val();
+                data.push(newsData);
+            });
+
+            //Though this will be called on a set interval, updating will only happen as neccessary.
+            this.setState({
+                newsEntries : data
+            });
+        })
+        .catch(err => {console.log(err);})
+    }
+
+    //Don't beleieve need to send any data, prob repeatedly call this on an interval instead of update, slightly better?
+    pushNewEntry = async() => {
+
+        const response = await fetch(url+"/NewsEntries",{
+            method:"POST",
+            
+
+        });
+
+        if (response.status.valueOf() != 200){
+            throw new Error("Failed to connect");
+        }
+
+        const body = await response.json();
+
+        return body;
+    }
+
 
     render(){
-        return <h1>hello</h1>
+        return (<div>
+                
+            
+            
+            </div>)
     }
 
 }
---
+++ ces_website/src/NewsEntry.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+
+class NewsEntry extends Component{
+
+    
+
+
+    constructor(props){
+
+        super(props);
+        
+        //Will think more on state for entries later.
+        this.state = {
+            beingEdited:false,
+
+        }
+    }
+
+    render(){
+
+        return (<div>
+                <h1 id="topic"> {this.props.topic}</h1>
+                <h3 id="author"> Written by {this.props.author} </h3>
+                <p id="description"> {this.props.desc }</p>
+            </div>);
+    }
+
+
+}
+
+export default NewsEntry; 
