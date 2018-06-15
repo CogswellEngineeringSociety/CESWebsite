aecd5b48adeb58c846c60ff52b623bdc2149a3e1
Prince Christian Basiga <princebasiga@gmail.com>
2018-06-14 21:33:15 -0700

    Updated Read me with notice of moving

--- ces_website/src/back-end/fire.js
+++ ces_website/src/back-end/fire.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase'
 
+
 var config = {
     apiKey: "AIzaSyADrVRU9CSIktkXnvQXcXFeOPicmYtC91M",
     authDomain: "ceswebsite-cf841.firebaseapp.com",
@@ -8,14 +9,9 @@ var config = {
     storageBucket: "ceswebsite-cf841.appspot.com",
     messagingSenderId: "612020639792"
   };
- 
+
   const fire = firebase.initializeApp(config);
-  
-  //Will add later when update domain, I know it works.
+  //Url to server app to send post requests to for functions not executed on client side. This app was deleted
   export const url = "https://middleman2.herokuapp.com"
   export default fire;
 
- 
-  //Contemplated having node back-end and the node code will have ref of firebase
-  //and do everything but I'm cutting the middleman. Will add if need
-  //for now will just have front-end use firebase api directly.
